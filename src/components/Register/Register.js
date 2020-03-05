59e861170a7890a86120c97ee73af8490eacbe41
Morgan <morgankclark@gmail.com>
2020-03-04 23:36:20 -0800

    completed the frontend - sign in form and routing

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
-const Signin = ({ onRouteChange }) => {
+const Register = ({ onRouteChange }) => {
     return(
         <article className="br3 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw6 center">
             <main className="pa4 black-80">
             <div className="measure">
                 <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-                <legend className="f1 fw6 ph0 mh0">Sign In</legend>
+                <legend className="f1 fw6 ph0 mh0">Register</legend>
                 <div className="mt3">
-                    <label className="db fw6 lh-copy f6" htmlfor="email-address">Email</label>
-                    <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address" />
+                    <label className="db fw6 lh-copy f6" htmlfor="name">Name</label>
+                    <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="text" name="name"  id="name" />
                 </div>
                 <div className="mv3">
                     <label className="db fw6 lh-copy f6" htmlfor="password">Password</label>
@@ -19,12 +19,9 @@ const Signin = ({ onRouteChange }) => {
                 <div className="">
                 <input 
                     onClick={() => onRouteChange('home')}
-                    className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" 
+                    className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib pointer" 
                     type="submit" 
-                    value="Sign in" />
-                </div>
-                <div className="lh-copy mt3">
-                <p onClick={() => onRouteChange('register')} className="f6 link dim black db pointer">Register</p>
+                    value="Register" />
                 </div>
             </div>
             </main>
@@ -32,4 +29,4 @@ const Signin = ({ onRouteChange }) => {
     );
 }
 
-export default Signin; 
+export default Register; 
