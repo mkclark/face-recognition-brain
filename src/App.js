f1ac23885b6620adaff422baade4ca107b35a777
Morgan <morgankclark@gmail.com>
2020-03-10 12:10:50 -0700

    working on connecting the server to the front-end

--- src/App.js
+++ src/App.js
@@ -51,6 +51,13 @@ class App extends Component {
     }
   }
 
+componentDidMount() {
+  fetch('http://localhost:3000')
+    .then(response => response.json())
+    .then(data => console.log(data))
+}
+
+
   calculateFaceLocation = (data) => {
     const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
     const image = document.getElementById('inputimage');
--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,35 +1,39 @@
 import React from 'react';
 
-const Signin = ({ onRouteChange }) => {
-    return(
-        <article className="br3 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw6 center">
-            <main className="pa4 black-80">
-            <div className="measure">
-                <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-                <legend className="f1 fw6 ph0 mh0">Sign In</legend>
-                <div className="mt3">
-                    <label className="db fw6 lh-copy f6" htmlfor="email-address">Email</label>
-                    <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address" />
+class Signin extends React.Component {
+    render () {
+        const { on RouteChange } = this.props;
+        return(
+            <article className="br3 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw6 center">
+                <main className="pa4 black-80">
+                <div className="measure">
+                    <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+                    <legend className="f1 fw6 ph0 mh0">Sign In</legend>
+                    <div className="mt3">
+                        <label className="db fw6 lh-copy f6" htmlfor="email-address">Email</label>
+                        <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address" />
+                    </div>
+                    <div className="mv3">
+                        <label className="db fw6 lh-copy f6" htmlfor="password">Password</label>
+                        <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password"  id="password" />
+                    </div>
+                    </fieldset>
+                    <div className="">
+                    <input 
+                        onClick={() => onRouteChange('home')}
+                        className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" 
+                        type="submit" 
+                        value="Sign in" />
+                    </div>
+                    <div className="lh-copy mt3">
+                    <p onClick={() => onRouteChange('register')} className="f6 link dim black db pointer">Register</p>
+                    </div>
                 </div>
-                <div className="mv3">
-                    <label className="db fw6 lh-copy f6" htmlfor="password">Password</label>
-                    <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password"  id="password" />
-                </div>
-                </fieldset>
-                <div className="">
-                <input 
-                    onClick={() => onRouteChange('home')}
-                    className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" 
-                    type="submit" 
-                    value="Sign in" />
-                </div>
-                <div className="lh-copy mt3">
-                <p onClick={() => onRouteChange('register')} className="f6 link dim black db pointer">Register</p>
-                </div>
-            </div>
-            </main>
-        </article>
-    );
+                </main>
+            </article>
+        );
+    }
+ 
 }
 
 export default Signin; 
