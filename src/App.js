b8f84ab381bb36eba8e8dcfffa0733c246f1be84
Morgan <morgankclark@gmail.com>
2020-03-21 19:01:24 -0700

    exporting handleApiCall function from image.js file

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Particles from 'react-particles-js';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Navigation from './components/Navigation/Navigation';
-import Signin from './components/Signin/Signin';
+import Signin from './components/Signin/SignIn';
 import Register from './components/Register/Register';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
